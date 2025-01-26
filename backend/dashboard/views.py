66b56ca4b3c3b79ecad6e4deff8f6cc1650ecd33
deJames <109223910+deJames-13@users.noreply.github.com>
2025-01-26 16:38:15 +0800

    cleaning

--- backend/dashboard/views.py
+++
@@ -1,4 +0,0 @@
-from django.views import generic
-from django.urls import reverse_lazy
-from . import models
-from . import forms
--- backend/face_recognition/urls.py
+++ backend/face_recognition/urls.py
@@ -6,14 +6,8 @@ from . import views
 
 
 router = routers.DefaultRouter()
-router.register("EmotionData", api.EmotionDataViewSet)
+router.register("emotion-data", api.EmotionDataViewSet)
 
 urlpatterns = (
-    path("api/v1/", include(router.urls)),
-    path("", views.EmotionDataListView.as_view(), name="face_recognition_EmotionData_list"),
-    path("create/", views.EmotionDataCreateView.as_view(), name="face_recognition_EmotionData_create"),
-    path("detail/<int:pk>/", views.EmotionDataDetailView.as_view(), name="face_recognition_EmotionData_detail"),
-    path("update/<int:pk>/", views.EmotionDataUpdateView.as_view(), name="face_recognition_EmotionData_update"),
-    path("delete/<int:pk>/", views.EmotionDataDeleteView.as_view(), name="face_recognition_EmotionData_delete"),
-
+    path("", include(router.urls)),
 )
