cfdd1562d4a98195bdae341157b6f7dcbeb22ccc
deJames <109223910+deJames-13@users.noreply.github.com>
2025-02-01 17:14:38 +0800

    asdf

--- backend/feedbacks/models.py
+++ backend/feedbacks/models.py
@@ -3,14 +3,20 @@ from django.db import models
 from django.urls import reverse
 from django.conf import settings
 
+
+class FeedbackCollection(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_nid = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 class Feedback(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    content = models.TextField(blank=True, null=True)
-    user_nid = models.TextField(blank=True, null=True)
-    
-        
     created = models.DateTimeField(auto_now_add=True, editable=False)
     last_updated = models.DateTimeField(auto_now=True, editable=False)
+    
+    
+    content = models.TextField(blank=True, null=True)
     
     class Meta:
         pass
