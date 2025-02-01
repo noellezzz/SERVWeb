c010049d9e89fd337e526f262c337beac73b495f
deJames <109223910+deJames-13@users.noreply.github.com>
2025-02-01 14:08:11 +0800

    adsfafsd

---
+++ frontend/src/layouts/admin/Sidebar/NavTitle.jsx
@@ -0,0 +1,9 @@
+const NavTitle = ({ item }) => {
+    return (
+        <div className="text-gray-500 text-sm uppercase font-semibold pt-2">
+            {item.title}
+        </div>
+    );
+};
+
+export default NavTitle;
---
+++ frontend/src/layouts/admin/Sidebar/navigations.jsx
@@ -0,0 +1,21 @@
+
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import NoteAltIcon from "@mui/icons-material/NoteAlt";
+import SettingsIcon from "@mui/icons-material/Settings";
+import GroupIcon from "@mui/icons-material/Group";
+import ReportIcon from "@mui/icons-material/Description";
+
+
+export const NAVIGATION = [
+    { type: "menu", segment: "", title: "Dashboard", icon: <DashboardIcon /> },
+    { type: "title", title: "Sentiment Analysis" },
+    { type: "menu", segment: "reports", title: "Reports", icon: <ReportIcon /> },
+    { type: "menu", segment: "assessments", title: "Assessments", icon: <NoteAltIcon /> },
+    { type: "menu", segment: "analytics", title: "Analytics", icon: <BarChartIcon /> },
+
+    { type: "title", title: "Manage", },
+    // { type: "menu", segment: "services", title: "Manage Services", icon: <GroupIcon /> },
+    { type: "menu", segment: "users", title: "Manage Users", icon: <GroupIcon /> },
+    { type: "menu", segment: "settings", title: "Settings", icon: <SettingsIcon /> },
+];
