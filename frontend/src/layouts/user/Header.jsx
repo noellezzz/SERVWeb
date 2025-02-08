44d470cf85a1097349f1a550f9061366f7665023
yazivonz <jazlynyvonne.baluarte@tup.edu.ph>
2025-02-08 09:53:14 +0800

    lgo

--- frontend/src/layouts/user/Header.jsx
+++ frontend/src/layouts/user/Header.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/SERV_Logo.png";
-import wordmark from "../../assets/Wordmark.png";
+import wordmark from "../../assets/Serv-adm.png";
 
 function Header() {
   return (
     <div className="px-4 shadow-lg">
       <div className="top-0 left-0 w-full p-5 flex gap-2 font-poppins">
         <div className="w-full text-2xl flex items-center gap-2">
-          <img src={logo} alt="Logo" className="w-12 h-12" />
-          <img src={wordmark} alt="Word Mark" className="h-12" />
+          <img src={logo} alt="Logo" className="w-14 h-14" />
+          <img src={wordmark} alt="Word Mark" className="h-9" />
         </div>
         <div className="w-full flex justify-end gap-6 items-center">
           <Link className="text-white" to="/">
