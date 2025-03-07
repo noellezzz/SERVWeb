5b235580c161d681cf38f59fc75be1a83149b3ee
noellezzz <angelomiguel.dacumos@tup.edu.ph>
2025-03-08 01:21:47 +0800

    jazzmin users and service + login protected routes

--- frontend/src/states/slices/auth.slice.js
+++ frontend/src/states/slices/auth.slice.js
@@ -1,28 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    user: null,
-    isAuthenticated: false,
-    loading: false,
-    token: localStorage.getItem('token') || null,
+  user: null,
+  isAuthenticated: !!localStorage.getItem('token'),
+  loading: false,
+  token: localStorage.getItem('token') || null,
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        loginSuccess: (state, action) => {
-            state.token = action.payload.token;
-            state.isAuthenticated = true;
-            localStorage.setItem('token', action.payload.token);
-        },
-        logout: (state) => {
-            state.token = null;
-            state.isAuthenticated = false;
-            localStorage.removeItem('token');
-        },
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginRequest: (state) => {
+      state.loading = true;
+      state.error = null;
     },
+    loginSuccess: (state, action) => {
+      state.token = action.payload.token;
+      state.isAuthenticated = true;
+      localStorage.setItem('token', action.payload.token);
+      state.loading = false;
+    },
+    loginFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logout: (state) => {
+      state.token = null;
+      state.isAuthenticated = false;
+      localStorage.removeItem('token');
+    },
+  },
 });
 
-export const { loginSuccess, logout } = authSlice.actions;
+export const { loginRequest, loginSuccess, loginFailure, logout } = authSlice.actions;
+
 export default authSlice.reducer;
