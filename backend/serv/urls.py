b5dde7e7daf35ca4c82cd920556df448b72461e3
deJames-13 <de.james013@gmail.com>
2025-03-25 20:06:08 +0800

    reroute admin

--- backend/serv/urls.py
+++ backend/serv/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
 
     # path('accounts/', include('django.contrib.auth.urls')),
     path('api/v1/', include([
